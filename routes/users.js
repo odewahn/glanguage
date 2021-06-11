f57bf179a52ff2b5438c543431e8e01d5a423961
Andrew Odewahn <odewahn@oreilly.com>
2021-06-10 21:49:33 -0400

    cleaning up comments

--- routes/users.js
+++ routes/users.js
@@ -2,10 +2,6 @@ const express = require("express");
 var router = express.Router();
 
 router.get("/", function (req, res, next) {
-  // Comment out this line:
-  //res.send('respond with a resource');
-
-  // And insert something like this instead:
   res.json([
     {
       id: 1,
